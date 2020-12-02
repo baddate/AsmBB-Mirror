7c69227dba17c18eb891148109f871923f783847
baddate <badgatex@outlook.com>
2020-12-02 15:11:33 +0800

    update the repo.

--- git.sh
+++ git.sh
@@ -6,13 +6,13 @@ echo -e "========================INSTALL FOSSIL"
 sudo apt install fossil
 
 echo -e "========================CLONE REPO"
-fossil help
+echo `fossil help`
 fossil clone https://asm32.info/fossil/repo/asmbb asmbb.fossil
 
 echo -e "========================MKDIR"
 mkdir asmbb
 cd asmbb
-echo -e "`pwd`"
+echo -e `pwd`
 echo -e "========================OPEN FOSSIL"
 fossil open ../asmbb.fossil
 
