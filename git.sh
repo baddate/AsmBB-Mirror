db03fdb06fc5b40389e54aa326262be4247a3cf7
baddate <badgatex@outlook.com>
2020-12-02 14:43:27 +0800

    1

--- git.sh
+++ git.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+echo "config git"
 git config --global user.name "baddate"
 git config --global user.email "badgatex@outlook.com"
-
+echo "install fossil"
 sudo apt install fossil
 
 fossil clone https://asm32.info/fossil/repo/asmbb asmbb.fossil
+echo "mkdir"
 mkdir asmbb
 cd asmbb
+echo "open fossil"
 fossil open ../asmbb.fossil
 
 git add .
