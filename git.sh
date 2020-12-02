eec9927c2d573f2242280be3c7b1e40c3658b37e
baddate <badgatex@outlook.com>
2020-12-02 18:40:16 +0800

    teeeeeeeee

--- git.sh
+++ git.sh
@@ -33,7 +33,7 @@ sudo ./fossil open asmbb.fossil --workdir /asmbb
 echo -e `pwd`
 echo -e "========================LS"
 sudo rm -f fossil fossil-linux-x64-2.13.tar.gz asmbb.fossil
-echo `ls`
+echo `ls -l`
 echo -e "========================MV"
 sudo mv asmbb/ . -f
 echo -e "========================GIT"
