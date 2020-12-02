4a28de10fb0852a8a09507fb9c68290cf099a706
baddate <37013819+baddate@users.noreply.github.com>
2020-12-02 17:25:15 +0800

    1234

--- git.sh
+++ git.sh
@@ -4,14 +4,17 @@ git config --global user.name "baddate"
 git config --global user.email "badgatex@outlook.com"
 echo -e "========================INSTALL FOSSIL"
 
-sudo wget -c https://www.fossil-scm.org/fossil/uv/fossil-src-2.13.tar.gz
-tar zxvf fossil-src-2.13.tar.gz
-cd fossil-2.13
-chmod +x configure
-./configure; make
+#sudo wget -c https://www.fossil-scm.org/fossil/uv/fossil-src-2.13.tar.gz
+#tar zxvf fossil-src-2.13.tar.gz
+#cd fossil-2.13
+sudo wget -c https://www.fossil-scm.org/fossil/uv/fossil-linux-x64-2.13.tar.gz
+tar zxvf fossil-linux-x64-2.13.tar.gz
+
+chmod a+x fossil
+
 echo -e "========================CLONE REPO"
 
-fossil clone https://asm32.info/fossil/repo/asmbb asmbb.fossil
+sudo fossil clone https://asm32.info/fossil/repo/asmbb asmbb.fossil
 
 echo -e "========================MKDIR"
 mkdir asmbb
